e16dd6d845613110048b46e74a18e1a40708f3c2
Muhammad Adnan Khalid <muhammad.adnan@systemsltd.com>
2021-10-27 23:48:03 +0500

    fix the issues in services

--- src/main/java/com/employee/api/service/impl/EmployeeServiceImp.java
+++ src/main/java/com/employee/api/service/impl/EmployeeServiceImp.java
@@ -1,15 +1,18 @@
 package com.employee.api.service.impl;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
+import org.apache.log4j.Logger;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 
+import com.employee.api.controller.EmployeeController;
 import com.employee.api.dao.DepartmentDao;
 import com.employee.api.dao.EmployeeDao;
 import com.employee.api.dao.ManagerDao;
@@ -23,6 +26,8 @@ import com.employee.api.service.EmployeeService;
 
 @Service
 public class EmployeeServiceImp implements EmployeeService {
+
+	private static final Logger LOGGER = Logger.getLogger(EmployeeController.class);
 
 	@Autowired
 	private EmployeeDao employeeDao;
@@ -48,7 +53,8 @@ public class EmployeeServiceImp implements EmployeeService {
 			employeeDto = modelMapper.map(employee, EmployeeDto.class);
 
 		} catch (Exception e) {
-			// TODO: handle exception
+
+			LOGGER.error("Exception occured in create employee ", e);
 		}
 
 		return employeeDto;
@@ -84,7 +90,7 @@ public class EmployeeServiceImp implements EmployeeService {
 			}
 
 		} catch (Exception e) {
-			// TODO: handle exception
+			LOGGER.error("Exception occured in update employee ", e);
 		}
 
 		return employeeDto;
@@ -93,29 +99,52 @@ public class EmployeeServiceImp implements EmployeeService {
 	@Override
 	public List<EmployeeDto> findAll(int page, int size) {
 
-		PageRequest pageReq = PageRequest.of(page, size);
+		List<EmployeeDto> employess = new ArrayList<>();
+		try {
 
-		Page<Employee> employees = employeeDao.findAll(pageReq);
+			PageRequest pageReq = PageRequest.of(page, size);
 
-		List<EmployeeDto> employess = employees.getContent().stream()
-				.map(emp -> modelMapper.map(emp, EmployeeDto.class)).collect(Collectors.toList());
+			Page<Employee> employees = employeeDao.findAll(pageReq);
+
+			employess = employees.getContent().stream().map(emp -> modelMapper.map(emp, EmployeeDto.class))
+					.collect(Collectors.toList());
+
+		} catch (Exception e) {
+			LOGGER.error("Exception occured in find all employee ", e);
+		}
 
 		return employess;
 	}
 
 	@Override
 	public void delete(int id) {
-		employeeDao.delete(id);
+
+		try {
+
+			employeeDao.delete(id);
+
+		} catch (Exception e) {
+
+			LOGGER.error("Exception occured in delete employee ", e);
+		}
 
 	}
 
 	@Override
 	public List<DepartmentDto> getAllDepartments() {
 
-		List<Department> departments = departmentDao.findAll();
+		List<DepartmentDto> departmentDtos = new ArrayList<>();
+		try {
 
-		List<DepartmentDto> departmentDtos = departments.stream()
-				.map(dept -> modelMapper.map(dept, DepartmentDto.class)).collect(Collectors.toList());
+			List<Department> departments = departmentDao.findAll();
+
+			departmentDtos = departments.stream().map(dept -> modelMapper.map(dept, DepartmentDto.class))
+					.collect(Collectors.toList());
+
+		} catch (Exception e) {
+
+			LOGGER.error("Exception occured in all departments ", e);
+		}
 
 		return departmentDtos;
 	}
@@ -123,22 +152,37 @@ public class EmployeeServiceImp implements EmployeeService {
 	@Override
 	public List<ManagerDto> getAllManagers() {
 
-		List<Manager> managers = managerDao.findAll();
+		List<ManagerDto> managerDtos = new ArrayList<ManagerDto>();
+		try {
 
-		List<ManagerDto> managerDtos = managers.stream().map(man -> modelMapper.map(man, ManagerDto.class))
-				.collect(Collectors.toList());
+			List<Manager> managers = managerDao.findAll();
+
+			managerDtos = managers.stream().map(man -> modelMapper.map(man, ManagerDto.class))
+					.collect(Collectors.toList());
+
+		} catch (Exception e) {
+			LOGGER.error("Exception occured in all managers ", e);
+		}
 
 		return managerDtos;
 	}
 
 	@Override
 	public EmployeeDto findById(int id) {
-		Optional<Employee> employee = employeeDao.findById(id);
 		EmployeeDto employeeDto = new EmployeeDto();
-		if (employee.isPresent()) {
+		try {
 
-			employeeDto = modelMapper.map(employee.get(), EmployeeDto.class);
+			Optional<Employee> employee = employeeDao.findById(id);
+
+			if (employee.isPresent()) {
+
+				employeeDto = modelMapper.map(employee.get(), EmployeeDto.class);
+			}
+
+		} catch (Exception e) {
+			LOGGER.error("Exception occured finding the employee by id ", e);
 		}
+
 		return employeeDto;
 	}
 
